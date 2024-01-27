fd46f73327c03dac4d57a7a9aefe9b06732b7ba1
Kaveri Chougule <chougulekaveri123@gmail.com>
2024-01-28 02:13:23 +0530

    Initial Commit

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useAuth } from "./Context/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const authCtx = useAuth();
+  const navigate = useNavigate();
   return (
     <div>
       Hello User!
+      <Button disabled={authCtx.loading} onClick={()=>{
+        authCtx.signOutFunc();
+        if(Object.keys(authCtx.currentUser).length === 0) navigate("/login")
+      }} className="w-100" type="submit">
+        Sign out
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
